a84471919ef990ae27c27e07a2e0fcbf09fe165a
Umar6501 <136899700+Umar6501@users.noreply.github.com>
2023-09-23 08:49:58 +0500

    hom63

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { logo, menu } from "../../assets";
 import "./Header.scss";
 
-export class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  toggleMenu = () => {
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }));
-  };
-  render() {
-    return (
-      <header className="header">
-        <div className="container-header">
-          <nav>
-            <a href="#" className="logo">
-              <img src={logo} alt="logo" />
-            </a>
-            <a href="#" onClick={toggleMenu} className="menu">
-              <img src={menu} alt="" />
-            </a>
-            <div className="big">
-              <div className="nav-links">
+const Header = () => {
+  const [open, setIsOpen] = useState(false);
+  return (
+    <header className="header">
+      <div className="container-header">
+        <nav>
+          <a href="#" className="logo">
+            <img src={logo} alt="logo" />
+          </a>
+          <button href="#" className="menu" onClick={() => setIsOpen(!open)}>
+            <img src={menu} alt="" />
+          </button>
+          <div className="big">
+            {open && (
+              <div className="nav-links open">
                 <p className="line"></p>
                 <a href="#">Продукция</a>
                 <p className="line"></p>
@@ -44,18 +33,83 @@ export class Header extends Component {
                 <a href="#">Контакты</a>
                 <p className="line"></p>
               </div>
-
-              <div className="dark">
-                <a href="#">
-                  <box-icon name="moon" className="dark" id="mooon"></box-icon>
-                </a>
-              </div>
+            )}
+            <div className="nav-link">
+              <p className="line"></p>
+              <a href="#">Продукция</a>
+              <p className="line"></p>
+              <a href="#">Сертификаты</a>
+              <p className="line"></p>
+              <a href="#">Наша команда</a>
+              <p className="line"></p>
+              <a href="#">О нас</a>
+              <p className="line"></p>
+              <a href="#">Новости</a>
+              <p className="line"></p>
+              <a href="#">Вакансии</a>
+              <p className="line"></p>
+              <a href="#">Контакты</a>
+              <p className="line"></p>
             </div>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-}
+            <div className="dark">
+              <a href="#">
+                <box-icon name="moon" className="dark" id="mooon"></box-icon>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
+
+// import React, { Component } from "react";
+// import { logo, menu } from "../../assets";
+// import "./Header.scss";
+
+// const Header = () => {
+//     const Menus =
+//   return (
+//     <header className="header">
+//       <div className="container-header">
+//         <nav>
+//           {/* <a href="#" className="logo">
+//               <img src={logo} alt="logo" />
+//             </a> */}
+//           <button href="#" className="menu" onClick={this.toggleMenu}>
+//             <img src={menu} alt="" />
+//           </button>
+//           <div className={`big ${isOpen ? "open" : ""}`}>
+//             <div className="nav-links">
+//               <p className="line"></p>
+//               <a href="#">{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">Сертификаты{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">Наша команда{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">О нас{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">Новости{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">Вакансии{items.value}</a>
+//               <p className="line"></p>
+//               <a href="#">Контакты{items.value}</a>
+//               <p className="line"></p>
+//             </div>
+
+//             {/* <div className="dark">
+//                 <a href="#">
+//                   <box-icon name="moon" className="dark" id="mooon"></box-icon>
+//                 </a>
+//               </div> */}
+//           </div>
+//         </nav>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
